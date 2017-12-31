8e3bc96505eddf229d948ff9a9bef840e2a962ee
Soxasora <simone@soxa.it>
2017-12-31 17:03:32 +0100

    final touches

--- ExchangeCoin/src/main/java/net/blockstreet/coin/Exchange.java
+++ ExchangeCoin/src/main/java/net/blockstreet/coin/Exchange.java
@@ -1,11 +1,6 @@
+// This enables redd's exchanges in-game
+
 package net.blockstreet.coin;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -31,73 +26,24 @@ public class Exchange extends JavaPlugin implements Listener {
 		getLogger().info("Exchanges between users, deactivated!");
 	}
 	
+	// This does things at the player's first login.
 	@EventHandler
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
 	  if(!event.getPlayer().hasPlayedBefore()) {
-	    String fileName = (event.getPlayer().getDisplayName() + ".txt");
-		    try {
-	            // Assume default encoding.
-	            FileWriter fileWriter =
-	                new FileWriter(fileName);
-	
-	            // Always wrap FileWriter in BufferedWriter.
-	            BufferedWriter bufferedWriter =
-	                new BufferedWriter(fileWriter);
-	
-	            // Note that write() does not automatically
-	            // append a newline character.
-	            bufferedWriter.write("0");
-	
-	            // Always close files.
-	            bufferedWriter.close();
-	            event.getPlayer().sendMessage("Now you have 0 Reddcoins, you can exchange Reddcoins with /rsend");
-	        }
-	        catch(IOException ex) {
-	            System.out.println(
-	                "Error writing to file '"
-	                + fileName + "'");
-	            // Or we could just do this:
-	            // ex.printStackTrace();
-	        }
+		  //do things
 	    }
 	}
-	
-    public void findFile(String name,File file)
-    {
-        File[] list = file.listFiles();
-        if(list!=null)
-        for (File fil : list)
-        {
-            if (fil.isDirectory())
-            {
-                findFile(name,fil);
-            }
-            else if (name.equalsIgnoreCase(fil.getName()))
-            {
-                System.out.println(fil.getParentFile());
-            }
-        }
-    }
 
-	// Il comando si esegue con /rsend nomeplayer soldi
+
+	// This does commands.
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		// Controllo iniziale prima del comando
 		
 	    if (sender instanceof Player) {
-	        Player player = (Player) sender;
-	        Player playerD = Bukkit.getPlayer(args[0]);
-	        try {    
-	        		sender.sendMessage("You don't have enough Reddcoins, you need " + (Integer.parseInt(args[1])-Integer.parseInt(lineM)) + " more Reddcoins.");
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				sender.sendMessage("Player not found, check what you've wrote.");
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				sender.sendMessage("Generic error, contact staff.");
-				e.printStackTrace();
-			}
+	        Player playerD = Bukkit.getPlayer(args[0]); // Player2
+	        	sender.sendMessage("Say things to the sender.");
+	        	playerD.sendMessage("Say things to the other player.");
 	    } else {
 	        sender.sendMessage("You must be a player!");
 	        return false;
