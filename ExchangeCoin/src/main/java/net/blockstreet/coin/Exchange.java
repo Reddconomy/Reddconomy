2fd0c0a0818cbbc5fd0e4e9f23f21101e709c63f
Soxasora <simone@soxa.it>
2018-01-04 01:10:49 +0100

    Comments added, fixed indentation

--- ExchangeCoin/src/main/java/net/blockstreet/coin/Exchange.java
+++ ExchangeCoin/src/main/java/net/blockstreet/coin/Exchange.java
@@ -1,64 +1,59 @@
-// This enables redd's exchanges in-game
-
+// This enables reddcoin exchanges in-game
+// Author: Simone Cervino, @soxasora
 package net.blockstreet.coin;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
-import java.util.UUID;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+//import java.util.UUID;
+//import org.bukkit.event.player.PlayerJoinEvent;
 public class Exchange extends JavaPlugin implements Listener {
-
 	
-	@Override
-	public void onEnable() {
-		// TODO what the plugin will do when enabled
-		getServer().getPluginManager().registerEvents(this, this);
-		//getLogger().info("onEnable has been invoked!");
-		getLogger().info("Exchanges between users, activated!");
-
-		
-	}
-	
-	@Override
-	public void onDisable() {
-		// TODO what the plugin will do when disabled
-		//getLogger().info("onDisable has been invoked!");
-		getLogger().info("Exchanges between users, deactivated!");
-	}
-	
-	// This does things at the player's first login.
-	@EventHandler
-	public void onPlayerJoinEvent(PlayerJoinEvent event) throws Throwable {
-	  if(!event.getPlayer().hasPlayedBefore()) {
-
-	    }
-	}
-	
+	// Dichiaro _JSON usando Gson
 	final Gson _JSON;
-	
 	public Exchange() {
 		_JSON = new GsonBuilder().setPrettyPrinting().create();
 	}
 	
+	// Cosa fa quando attivato
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("Exchanges between users, activated!");
+	}
+	
+	// Cosa fa quando disattivato
+	@Override
+	public void onDisable() {
+		getLogger().info("Exchanges between users, deactivated!");
+	}
+	
+	// Fa cose al primo login di un nuovo utente
+	/*@EventHandler
+	public void onPlayerJoinEvent(PlayerJoinEvent event) throws Throwable {
+	  if(!event.getPlayer().hasPlayedBefore()) {
+
+	    }
+	}*/
+	
+	// Le api fondamentali per interfacciarsi a Reddconomy.
+	@SuppressWarnings("rawtypes")
 	private Map apiCall(String action) throws Exception
 	{
 		  String urlString = "http://localhost:8099/?action=" + action;
 		  URL url = new URL(urlString);
 		  HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		 
-		  // Reading response from input Stream
-		  BufferedReader in = new BufferedReader(
-		          new InputStreamReader(con.getInputStream()));
+		  
+		  BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		  String output;
 		  StringBuffer response = new StringBuffer();
 		 
@@ -71,45 +66,55 @@ public class Exchange extends JavaPlugin implements Listener {
 		  return resp;
 	}
 	
-
-	 private String getAddrDeposit(long balance, String pUUID) throws Exception {
+	// Ottiene l'indirizzo per il deposito
+	private String getAddrDeposit(long balance, String pUUID) throws Exception
+	{
 		 
-		  String action = "deposit&wallid=" + pUUID + "&balance=" + balance;
-		  Map data=(Map)apiCall(action).get("data");
-		  return (String)data.get("addr");
-	 }
-	 
-	 private String createContract(long ammount, String pUUID) throws Exception
-	 {
-		 String action = "newcontract&wallid=" + pUUID + "&ammount=" + ammount;
+		 String action = "deposit&wallid=" + pUUID + "&balance=" + balance;
 		 Map data=(Map)apiCall(action).get("data");
-		 return (String)data.get("contractId");
-	 }
+		 return (String)data.get("addr");
+	}
+	
+	// Crea contratto
+	private String createContract(long ammount, String pUUID) throws Exception
+	{
+		String action = "newcontract&wallid=" + pUUID + "&ammount=" + ammount;
+		Map data=(Map)apiCall(action).get("data");
+		return (String)data.get("contractId");
+	}
 	 
-	 private void acceptContract(String contractId, String pUUID) throws Exception
-	 {
-		 String action = "acceptcontract&wallid=" + pUUID + "&contractid=" + contractId;
-		 apiCall(action);
-	 }
+	// Accetta contratto
+	private void acceptContract(String contractId, String pUUID) throws Exception
+	{
+		apiCall("acceptcontract&wallid=" + pUUID + "&contractid=" + contractId);
+	}
 	 
-	 private double getBalance(String pUUID) throws Exception {
-		 
-		  String action = "balance&wallid=" + pUUID;
-		  Map data=(Map)apiCall(action).get("data");
-		  Number balance=(Number)data.get("balance");
-		  return (balance.longValue())/100000000.0;
-	 }
+	// Ottiene saldo di un utente
+	private double getBalance(String pUUID) throws Exception
+	{
+		String action = "balance&wallid=" + pUUID;
+		Map data=(Map)apiCall(action).get("data");
+		Number balance=(Number)data.get("balance");
+		return (balance.longValue())/100000000.0;
+	}
 	 
-	// This does commands.
+	// Dichiarazione dei comandi
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-	// Controllo iniziale prima del comando
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		// Ti chiederai perché usare una stringa per l'UUID, beh, se il server è crackato
+		// usa il nome di un utente al posto di un UUID dato da Mojang.
+		String pUUID = null;
+		
+		// Controllo iniziale prima del comando
+		// I comandi dunque funzionano solo se eseguiti da utenti.
 	    if (!(sender instanceof Player))
 	    {
 		    sender.sendMessage("You must be a player!");
 		    return false;
 	    }
-	    String pUUID = null;
+	    
+	    // Se il server è originale, usa UUID, altrimenti usa nome del player.
 	    if ((getServer().getOnlineMode())==true)
 	    {
 	    	pUUID = (((Player) sender).getUniqueId()).toString();
@@ -117,6 +122,7 @@ public class Exchange extends JavaPlugin implements Listener {
 	    	pUUID = sender.getName();
 	    }
 	    	
+	    // Comando /deposit | Mostra l'indirizzo sul quale l'utente deve depositare
 		if (cmd.getName().equalsIgnoreCase("deposit"))
 		{
 		    if (args.length==1) {
@@ -133,7 +139,8 @@ public class Exchange extends JavaPlugin implements Listener {
 	    			return false;
 		    }
 		}
-	
+		
+		// Comando /balance | Mostra il saldo di un utente
 		else if (cmd.getName().equalsIgnoreCase("balance"))
 	    {		    	
 		    if (args.length==0)
@@ -151,6 +158,7 @@ public class Exchange extends JavaPlugin implements Listener {
 			}
 	    }
 	    
+		// Comando /contract | Crea e accetta contratti
 		else if (cmd.getName().equalsIgnoreCase("contract"))
 	    {		    	
 			if (args.length==2)
@@ -178,8 +186,6 @@ public class Exchange extends JavaPlugin implements Listener {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-		    		/*} else if (args[0].equalsIgnoreCase("info")) {
-		    			String contractId = args[1];*/
 		    		}
 				return true;
 			} else {
