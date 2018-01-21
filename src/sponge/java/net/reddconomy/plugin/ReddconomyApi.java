cd565038f46221604daed56d3c4e69af00e80667
Soxasora <simone@soxa.it>
2018-01-21 18:23:10 +0100

    ReddconomyApi now uses only long amounts instead of a mix of them

--- src/sponge/java/net/reddconomy/plugin/ReddconomyApi.java
+++ src/sponge/java/net/reddconomy/plugin/ReddconomyApi.java
@@ -144,9 +144,10 @@ public class ReddconomyApi{
 
 
 	// QR Engine
-	public static String createQR(String addr, String coin, double amount) throws WriterException {
+	public static String createQR(String addr, String coin, long amount) throws WriterException {
+		double damount = reddconomy.Utils.convertToUserFriendly(amount);
 		Map<EncodeHintType,Object> hint=new HashMap<EncodeHintType,Object>();
-		com.google.zxing.qrcode.encoder.QRCode code=Encoder.encode(coin!=null&&!coin.isEmpty()?coin+":"+addr+"?amount="+amount:""+amount,ErrorCorrectionLevel.L,hint);
+		com.google.zxing.qrcode.encoder.QRCode code=Encoder.encode(coin!=null&&!coin.isEmpty()?coin+":"+addr+"?amount="+damount:""+damount,ErrorCorrectionLevel.L,hint);
 		ByteMatrix matrix=code.getMatrix();
 		System.out.println(matrix.getWidth()+"x"+matrix.getHeight());
 		StringBuilder qr=new StringBuilder();
